1a0ba2b5a9cc5b2af20647619a3d54cb0d258883
AndyGarcia <andyg.oyarzo@gmail.com>
2023-11-12 14:08:13 -0300

    Recordar modificar base de datos, esta debemos sicronizarla

--- backend/settings.py
+++ backend/settings.py
@@ -85,9 +85,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'andyg',
+        'PASSWORD': '2022Anto-+',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-
+    path('users/', include('users.urls'))
 
 ]
 
